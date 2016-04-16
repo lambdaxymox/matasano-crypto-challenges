5ec522891f0df917631bab7a1caf0f90bd59cc2c
LambdaXymox <lambda.xymox@gmail.com>
2016-04-16 15:06:10 -0500

    Complete Challenge 7.

--- src/Set1/Challenge7.hs
+++ src/Set1/Challenge7.hs
@@ -1,14 +1,20 @@
-module Set1.Challenge7 where
+module Set1.Challenge7
     (
         secret,
         secretKey,
+        cipher,
         challenge7,
     )
     where
 
-import Data.ByteString        as BS
-import Data.ByteString.Char8  as BSC8
-import Data.ByteString.Base64 as Base64
+import           Util.IO                (readBS)
+import           Data.ByteString        as BS
+import           Data.ByteString.Char8  as BSC8
+import           Data.ByteString.Base64 as Base64
+import qualified Crypto.Cipher.AES      as AES
+import           Crypto.Cipher.Types
+import           Crypto.Error
+import           Data.Maybe
 
 
 secret :: IO BS.ByteString
@@ -17,5 +23,8 @@ secret = Base64.decodeLenient <$> readBS "Set1/ex7.txt"
 secretKey :: BS.ByteString
 secretKey = BSC8.pack "YELLOW SUBMARINE"
 
+cipher :: AES.AES128
+cipher = fromJust $ maybeCryptoError $ cipherInit secretKey
+
 challenge7 :: IO BS.ByteString
-challenge7 = return BS.empty
+challenge7 = ecbDecrypt cipher <$> secret
